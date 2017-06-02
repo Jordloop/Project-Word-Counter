17a1959ed744d8e823c6a958d185f278f18dcc89
JordLoop <Jordloop@gmail.com>
2017-06-02 10:37:31 -0700

    add a failing first test on CountRepeatsTest.cs.

---
+++ Objects/RepeatCounter.cs
@@ -0,0 +1,24 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace WordCounter.Objects
+{
+  public class RepeatCounter
+  {
+    private string _userWord;
+    private string _userSentence;
+
+    public RepeatCounter(string word, string sentence)
+    {
+      _userWord = word;
+      _userSentence = sentence;
+    }
+
+    public int CountRepeats(string userWord, string userSentence)
+    {
+
+      return 0;
+    }
+  }
+}
---
+++ Tests/CountRepeatsTest.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using WordCounter.Objects;
+
+namespace WordCounter
+{
+  public class CountRepeatsTest
+  {
+    [Fact]
+    public void CheckEquality_CountRepeats()
+    {
+      //ARRANGE
+      int expectedResult = 1;
+      string testWord = "Foo";
+      string testSentence = "Foo";
+      RepeatCounter newCount = new RepeatCounter(testWord, testSentence);
+      //ACT
+      int result = newCount.CountRepeats(testWord, testSentence);
+      //ASSERT
+      Assert.Equal(expectedResult, result);
+    }
+  }
+}
