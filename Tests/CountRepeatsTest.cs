55cf7d6ea763cde49a5dd65423b3dae08044510a
JordLoop <Jordloop@gmail.com>
2017-06-02 11:55:39 -0700

    add a passing fifth test on CountRepeatsTest.cs.

--- Tests/CountRepeatsTest.cs
+++ Tests/CountRepeatsTest.cs
@@ -49,13 +49,28 @@ namespace WordCounter
       //ASSERT
       Assert.Equal(expectedResult, result);
     }
+
     [Fact]
-    public void CountRepeats_FindEqualityInMultiWordStringWithPunctuation_True()
+    public void CountRepeats_FindEqualityWithPunctuation_True()
     {
       //ARRANGE
       int expectedResult = 1;
       string testWord = "foo";
       string testSentence = "Why do I keep typing foo?";
+      RepeatCounter newCount = new RepeatCounter(testWord, testSentence);
+      //ACT
+      int result = newCount.CountRepeats(testWord, testSentence);
+      //ASSERT
+      Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void CountRepeats_FindEachEqualityInMultiWordString_True()
+    {
+      //ARRANGE
+      int expectedResult = 2;
+      string testWord = "foo";
+      string testSentence = "Foo, why do I keep typing foo?";
       RepeatCounter newCount = new RepeatCounter(testWord, testSentence);
       //ACT
       int result = newCount.CountRepeats(testWord, testSentence);
